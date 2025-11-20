f6f3ead23e610d2e4c9ac528f3f4c2a8846f4278
asoutrelle <asoutrelleperez@alumnos.exa.unicen.edu.ar>
2025-11-20 20:24:48 -0300

    ultimo commit

--- views/folder_templ.go
+++ views/folder_templ.go
@@ -45,14 +45,14 @@ func FolderList(folders []db.Folder) templ.Component {
 			return templ_7745c5c3_Err
 		}
 		for _, folder := range folders {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<a class=\"folder-item\" href=\"")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<a class=\"folder-item button\" href=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var2 templ.SafeURL
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinURLErrs("/folders/" + strconv.Itoa(int(folder.ID)))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/folder.templ`, Line: 17, Col: 95}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/folder.templ`, Line: 17, Col: 102}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
